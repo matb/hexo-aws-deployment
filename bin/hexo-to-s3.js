ebdcba9e3e13f7b001868704ec13433790d03439
Matthias Baumann <matthias@Matthiass-MacBook-Pro.local>
2016-05-01 14:32:46 +0200

    functional implementation

---
+++ bin/hexo-to-s3.js
@@ -0,0 +1,47 @@
+#! /usr/bin/env node
+
+'use strict';
+
+var minimist = require('minimist');
+var fs = require('fs');
+
+var argv = minimist(process.argv.slice(2), {string: 'bucket'});
+
+var bucket = argv.bucket;
+if (!bucket) {
+    throw 'Bucket name needs to be provided. Use --bucket.'
+}
+
+if (!fs.existsSync('public')) {
+    throw 'Public folder not found. Nothing to deploy. Consider calling hexo generate first.'
+}
+
+console.log('Start deploying to ' + bucket);
+
+var s3 = require('s3');
+var AWS = require('aws-sdk');
+var awsS3Client = new AWS.S3();
+var options = {
+    s3Client: awsS3Client
+};
+var client = s3.createClient(options);
+var params = {
+    localDir: 'public',
+    deleteRemoved: true, // default false, whether to remove s3 objects
+                         // that have no corresponding local file.
+
+    s3Params: {
+        Bucket: bucket,
+        ACL: 'public-read'
+    }
+};
+var uploader = client.uploadDir(params);
+uploader.on('error', function (err) {
+    console.error('Error while uploading: ', err.stack);
+});
+uploader.on('fileUploadEnd', function (localFilePath) {
+    console.log('Uploaded: ' + localFilePath)
+});
+uploader.on('end', function () {
+    console.log('Finished deploying to ' + bucket);
+});
